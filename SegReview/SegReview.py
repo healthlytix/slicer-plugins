8ef498c7579036c13c8861d3a89096bba63d67af
Brian Keating <bkeating@healthlytix.com>
2021-04-22 13:33:32 -0700

    remove nonfunctional unit test, removed save-seg functionality

--- SegReview/SegReview.py
+++ SegReview/SegReview.py
@@ -10,11 +10,11 @@ from slicer.ScriptedLoadableModule import *
 import logging
 
 
-class BatchSegmenter(ScriptedLoadableModule):
+class SegReview(ScriptedLoadableModule):
 
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
-        self.parent.title = "Batch Segmentation Editor"
+        self.parent.title = "Segmentation reviewer"
         self.parent.categories = ["Segmentation"]
         self.parent.dependencies = []
         self.parent.contributors = ["Brian Keating (Cortechs.ai)"]
@@ -23,17 +23,13 @@ class BatchSegmenter(ScriptedLoadableModule):
         self.parent.acknowledgementText = """"""
 
 
-"""
-Possibly relevant example: https://github.com/Slicer/Slicer/blob/a18612bb2584018822347ff4db16439b5c578e00/Utilities/Templates/Modules/Scripted/TemplateKey.py#L104-L108
-Use VTKObservationMixin and removeObservers()
-"""
-class BatchSegmenterWidget(ScriptedLoadableModuleWidget):
+class SegReviewWidget(ScriptedLoadableModuleWidget):
 
     def setup(self):
         ScriptedLoadableModuleWidget.setup(self)
         
         # Read config
-        config_fn = os.path.join(os.path.dirname(__file__), 'batch-segmenter-config.json')
+        config_fn = os.path.join(os.path.dirname(__file__), 'roi-definitions.json')
         print('Loading config from ', config_fn)
         with open(config_fn) as f:
             self.config = json.load(f)
@@ -196,10 +192,6 @@ class BatchSegmenterWidget(ScriptedLoadableModuleWidget):
         if not self.image_label_dict:
             return
 
-        # save old seg before loading the new one
-        if self.segmentationNode:
-            self.saveActiveSegmentation()
-
         try:
             self.selected_image_ind = list(self.image_label_dict.keys()).index(text)
         except ValueError:
@@ -252,7 +244,7 @@ class BatchSegmenterWidget(ScriptedLoadableModuleWidget):
 
 
     def createSegmentationFromFile(self, label_fn):
-        print('INFO: BatchSegmenter.createSegmentationFromFile invoked', label_fn)
+        print('INFO: SegReview.createSegmentationFromFile invoked', label_fn)
 
         # create label node as a labelVolume
         labelmapNode = slicer.util.loadLabelVolume(label_fn)
@@ -299,42 +291,8 @@ class BatchSegmenterWidget(ScriptedLoadableModuleWidget):
                 segmentation.AddEmptySegment(str(labelVal), labelName, color)
                 
 
-    def saveActiveSegmentation(self):
-        if self.active_label_fn:
-            print('INFO: BatchSegmenter.saveActiveSegmentation() invoked', self.active_label_fn)
-
-            # restore original label values
-            segmentation = self.segmentationNode.GetSegmentation()
-            for segInd in range(segmentation.GetNumberOfSegments()):
-                segment = segmentation.GetNthSegment(segInd)
-                try:
-                    labelVal = self.labelNameToLabelVal[segment.GetName()]
-                    print('INFO: saving '+segment.GetName()+' segment as '+str(labelVal))
-                    segment.SetName(str(labelVal))
-                except KeyError:
-                    # TODO: create user-visible error here (an alert box or something)
-                    print('ERROR: saving segment number '+str(segInd)+' failed because its name ("'+str(segment.GetName())+'") is not one of '+str(self.labelNameToLabelVal.keys()))
-                    return
-
-            # Save to file
-            if self.segmentationNode and self.segmentationNode.GetDisplayNode():
-                print('Saving seg to', self.active_label_fn)
-
-                # make a list of segment IDs *that are always ordered correctly*
-                visibleSegmentIds = vtk.vtkStringArray()
-                for labelVal in self.config['labelNames']:  # assumes that the config list of labelNames is in order
-                    visibleSegmentIds.InsertNextValue(labelVal)
-                visibleSegmentIdsList = [visibleSegmentIds.GetValue(ind) for ind in range(visibleSegmentIds.GetNumberOfValues())]
-                
-                labelmapNode = slicer.vtkMRMLLabelMapVolumeNode()
-                slicer.mrmlScene.AddNode(labelmapNode)
-                slicer.vtkSlicerSegmentationsModuleLogic.ExportSegmentsToLabelmapNode(self.segmentationNode, visibleSegmentIds, labelmapNode, self.volNodes[0])
-                slicer.util.saveNode(labelmapNode, self.active_label_fn)
-                slicer.mrmlScene.RemoveNode(labelmapNode)
-                        
-
     def clearNodes(self):
-        print('INFO: BatchSegmenter.clearNodes invoked')
+        print('INFO: SegReview.clearNodes invoked')
         for volNode in self.volNodes:
             slicer.mrmlScene.RemoveNode(volNode)
         if self.segmentationNode:
@@ -344,9 +302,7 @@ class BatchSegmenterWidget(ScriptedLoadableModuleWidget):
 
                 
     def cleanup(self):
-        print('INFO: BatchSegmenter.cleanup() invoked')
-        if self.segmentationNode:
-            self.saveActiveSegmentation()
+        print('INFO: SegReview.cleanup() invoked')
         self.clearNodes()
 
 
@@ -356,93 +312,3 @@ def loadLabelArrayFromFile(labelFilename):
     labelArray = slicer.util.arrayFromVolume(labelmapNode)
     slicer.mrmlScene.RemoveNode(labelmapNode)
     return labelArray
-
-
-class BatchSegmenterTest():
-
-    def delayDisplay(self, message, msec=150):
-        """Display a small dialog and wait
-    
-        This does two things: 1) it lets the event loop catch up
-        to the state of the test so that rendering and widget updates
-        have all taken place before the test continues and 2) it
-        shows the user/developer/tester the state of the test
-        so that we'll know when it breaks
-        """
-        print('TEST:', message)
-        self.info = qt.QDialog()
-        self.infoLayout = qt.QVBoxLayout()
-        self.info.setLayout(self.infoLayout)
-        self.label = qt.QLabel(message,self.info)
-        self.infoLayout.addWidget(self.label)
-        qt.QTimer.singleShot(msec, self.info.close)
-        self.info.exec_()
-
-
-    def setUp(self):
-        """ Do whatever is needed to reset the state - typically a scene clear will be enough."""
-        slicer.mrmlScene.Clear(0)
-
-
-    def runTest(self):
-        """Run as few or as many tests as needed here."""
-        self.setUp()
-        self.testBatchSegmenter()
-
-
-    def testBatchSegmenter(self):
-        self.delayDisplay('BatchSegmenter tests')
-
-        self.delayDisplay('Creating BatchSegmenter widgets')
-        
-        mainWindow = slicer.util.mainWindow()
-        mainWindow.moduleSelector().selectModule('BatchSegmenter')
-        batchSegmentationWidget = slicer.modules.BatchSegmenterWidget
-
-        testDataDir = os.path.join(os.path.dirname(__file__), 'Data')
-        sampleVolFilenames = [
-            os.path.join(testDataDir, 'T1-postcontrast.nii'),
-            os.path.join(testDataDir, 'T2.nii'),
-            os.path.join(testDataDir, 'FLAIR.nii'),
-            os.path.join(testDataDir, 'T1-precontrast.nii')
-        ]
-        tempdir = tempfile.mkdtemp()
-
-        # test round-trip with all segments
-        sampleLabelFilename = os.path.join(testDataDir, 'tumor-seg.nii')
-        originalSeg = loadLabelArrayFromFile(sampleLabelFilename)
-        batchSegmentationWidget.loadVolumesFromFiles(sampleVolFilenames)
-        batchSegmentationWidget.createSegmentationFromFile(sampleLabelFilename)
-        testSegFilename = os.path.join(tempdir, 'tumor-seg-test.nii')
-        batchSegmentationWidget.active_label_fn = testSegFilename
-        batchSegmentationWidget.saveActiveSegmentation()
-        finalSeg = loadLabelArrayFromFile(testSegFilename)
-        try:
-            np.testing.assert_array_equal(originalSeg, finalSeg)
-            self.delayDisplay('Round trip segmentation read-write-read test 1 successful')
-        except AssertionError as e:
-            self.delayDisplay('Round trip segmentation read-write-read test 1 FAILED')
-            raise e
-
-        batchSegmentationWidget.clearNodes()
-
-        # test round-trip with segments labeled 1, 3 (no edema/label 2 ROI)
-        sampleLabelFilename = os.path.join(testDataDir, 'tumor-seg-missing-edema.nii')
-        originalSeg = loadLabelArrayFromFile(sampleLabelFilename)
-        batchSegmentationWidget.loadVolumesFromFiles(sampleVolFilenames)
-        batchSegmentationWidget.createSegmentationFromFile(sampleLabelFilename)
-        testSegFilename = os.path.join(tempdir, 'tumor-seg-test.nii')
-        batchSegmentationWidget.active_label_fn = testSegFilename
-        batchSegmentationWidget.saveActiveSegmentation()
-        finalSeg = loadLabelArrayFromFile(testSegFilename)
-        try:
-            np.testing.assert_array_equal(originalSeg, finalSeg)
-            self.delayDisplay('Round trip segmentation read-write-read test 2 successful')
-        except AssertionError as e:
-            self.delayDisplay('Round trip segmentation read-write-read test 2 FAILED')
-            raise e
-
-        batchSegmentationWidget.clearNodes()
-        
-        self.delayDisplay('Tests passed!')
-    
